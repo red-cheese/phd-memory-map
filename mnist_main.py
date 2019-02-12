f5ddf52c9b1befefc16f0d5db131d82a7d2215bd
Alexandra Mikhailova <mikhaylova.alexandra.a@gmail.com>
2019-02-12 16:40:58 +0000

    Some more results for the 2-class MNIST experiment + Further research questions

--- mnist_main.py
+++ mnist_main.py
@@ -18,10 +18,13 @@ from ml import evaluate
 BATCH_SIZE = 64
 
 
-def classify_2(digit_A, digit_B):
+def classify_2(digit_A, digit_B, num_batches_A, num_batches_B):
     """
-    Alternating batches that consist solely of one of the two given digits.
-    10 batches of A -> 10 batches of B -> 10 of A -> 10 of B -> ...
+    Alternating batches that consist solely of one of the two given digits:
+        num_batches_A batches of A ->
+        num_batches_B batches of B ->
+        num_batches_A of A ->
+        num_batches_B of B -> ...
 
     Experiment design:
       - Initialise a 2-class classifier based on a Dense NN.
@@ -36,20 +39,22 @@ def classify_2(digit_A, digit_B):
     train_B_data = mnist_get_data.get_class_data(digit_B, train=True, batch_size=BATCH_SIZE, max_num=3000)
 
     # Combine into one training dataset.
-    n_batches = 20  # Will do n_batches of A, then n_batches of B, ...
     min_len = min(train_A_data.shape[0], train_B_data.shape[0])
-    i = 0
+    i_A = 0
+    i_B = 0
     train_data = np.array([], dtype=np.int64).reshape((0, train_A_data.shape[1]))
     train_labels = np.array([], dtype=np.int64).reshape((0, 2))
-    while i < min_len:
-        next_i = min(min_len, i + BATCH_SIZE * n_batches)
-        add_data_A = train_A_data[i:next_i, :]
-        add_data_B = train_B_data[i:next_i, :]
+    while i_A < min_len and i_B < min_len:
+        next_i_A = min(min_len, i_A + BATCH_SIZE * num_batches_A)
+        next_i_B = min(min_len, i_B + BATCH_SIZE * num_batches_B)
+        add_data_A = train_A_data[i_A:next_i_A, :]
+        add_data_B = train_B_data[i_B:next_i_B, :]
         train_data = np.vstack((train_data, add_data_A, add_data_B))
-        add_labels_A = mnist_get_data.generate_class_labels(digit_A, next_i - i, 2)
-        add_labels_B = mnist_get_data.generate_class_labels(digit_B, next_i - i, 2)
+        add_labels_A = mnist_get_data.generate_class_labels(digit_A, next_i_A - i_A, 2)
+        add_labels_B = mnist_get_data.generate_class_labels(digit_B, next_i_B - i_B, 2)
         train_labels = np.vstack((train_labels, add_labels_A, add_labels_B))
-        i = next_i
+        i_A = next_i_A
+        i_B = next_i_B
 
     dense_nn = dense.DenseNN(input_dim=train_A_data.shape[1], classes=[digit_A, digit_B], batch_size=BATCH_SIZE)
     dense_nn.fit(train_data, train_labels)
@@ -60,7 +65,7 @@ def classify_2(digit_A, digit_B):
 
 
 def main():
-    classify_2(0, 1)
+    classify_2(digit_A=0, digit_B=1, num_batches_A=1, num_batches_B=1)
 
 
 if __name__ == '__main__':
